5487acf12cbc8f192ea1d867fbc610b3135e7b61
JULIO RODRIGUEZ <julrodri@uat.edu>
2018-04-25 10:46:33 -0700

    Done with Files

---
+++ Drink Language/Commands.h
@@ -0,0 +1,8 @@
+//start of the header file 
+#pragma once
+#include <string>
+//Instead of using cout to print out text, here we changed cout to print, so everytime we type print, we can print text out to the user 
+void print(std::string printThis);
+//Replacing the cin
+std::string hear(std::string PromptUser); 
+#pragma once
